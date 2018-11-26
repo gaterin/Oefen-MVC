bf4952331696364d51b06bdc434866f53b9b3ea7
Georgi Atanasov <gaterin98@gmail.com>
2018-11-26 14:40:42 +0100

    config file added

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 require 'Controller/MainController.php';
 require 'Router/Router.php';
-
+require 'config.php';
 require 'Model/MainModel.php';
 
 // echo "<pre>";
 // <pre> does weird things to the website layout.
 // only use <pre> to display var_dump() output correctly on screen!!
+
+var_dump(APP_DIR);
 include "View/Header.php";
 $router = new Router();
 include "View/Footer.php";

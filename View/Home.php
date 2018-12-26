7b5659defff45d5958b389d59a71d1862c0f5ece
Frank-4a5a <449758@edu.rocmn.nl>
2018-12-26 01:12:09 +0100

    nieuwe banner

--- View/Home.php
+++ View/Home.php
@@ -3,5 +3,12 @@
 <div class="row">
   <div class="col-12">
     <h1>HomePage</h1>
+    <div class="col-2">
+
+    </div>
+    <img class="col-8" src="./Images/HomeDepot.jpg">
+    <div class="col-2">
+
+    </div>
   </div>
 </div>

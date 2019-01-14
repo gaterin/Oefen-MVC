83622fc30a014962f58f835d469db551722daf64
Frank-4a5a <449758@edu.rocmn.nl>
2019-01-14 20:26:01 +0100

    added login form to html with border switch button

--- View/Header.php
+++ View/Header.php
@@ -4,12 +4,14 @@
     <meta charset="utf-8">
     <link type="text/css" href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <base href="http://localhost/php/Oefen-MVC/"/>
+    <link rel="shortcut icon" href="Images/Logo.png" type="image/png" />
     <link type="text/css" rel="stylesheet" href="./Style/GridStyle.css">
     <link type="text/css" rel="stylesheet" href="./Style/MainStyle.css">
     <link type="text/css" rel="stylesheet" href="./Style/HeaderStyle.css">
     <link type="text/css" rel="stylesheet" href="./Style/FooterStyle.css">
     <title></title>
   </head>
+  <button type="button" id="BorderSwitchId" onclick="GridBorderSwitch();">Switch borders on/off</button>
   <body>
     <div class="row">
       <div class="col-12">
@@ -28,7 +30,33 @@
           </div>
         </div>
         <div class="col-2">
+          <?php
+          // vardump($_SESSION["msg"]);
+          if (isset($_SESSION["msg"]))
+          {
+            echo $_SESSION["msg"];
+            unset($_SESSION["msg"]);
+          }
+          ?>
+
+          <?php
+          if (!isset($_SESSION["username"]))
+          {  ?>
+
           <h1>Login</h1>
+          <form class="" action="./LoginController/login" method="post">
+            <input type="text" name="username" placeholder="Enter username..." required>
+            <input type="text" name="password" placeholder="Enter password..." required>
+            <input type="submit" name="loginbool" value="Login">
+          </form>
+        <?php }else {  ?>
+          <h1>Logout</h1>
+          <p>logged in as: <?php echo $_SESSION["username"];?></p>
+          <form class="" action="./LoginController/logout" method="post">
+            <input type="submit"  value="Logout">
+          </form>
+          <?php } ?>
+
         </div>
 
       </div>

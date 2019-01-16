4b0ed45180979e64c023000978f750b7cf2bd2bc
Frank-4a5a <449758@edu.rocmn.nl>
2019-01-16 16:39:53 +0100

    Update EditStock.php

--- View/EditStock.php
+++ View/EditStock.php
@@ -1,7 +1,12 @@
-
+<p class="col-3">Product:</p>
+<p class="col-3">Location:</p>
+<p class="col-3">Amount in stock:</p>
 <form class="col-12" action="./StockController/editStock" method="post">
-<input class="col-3" name="productId" value="<?php echo $productId; ?>" placeholder="<?php echo $productId; ?>" readonly>
-<input class="col-3" name="locationId" value="<?php echo $locationId; ?>" placeholder="<?php echo $locationId; ?>" readonly>
+
+<input class="col-3" value="<?php echo $productName; ?>"  readonly>
+<input class="col-3" value="<?php echo $locationName; ?>"  readonly>
 <input class="col-3" name="amountInStock"  value="<?php echo $amountInStock; ?>" placeholder="Amount in stock">
-<input class="col-3"type="submit" value="Update stock!">
+<input class="col-3" type="hidden" name="productId" value="<?php echo $productId; ?>" readonly>
+<input class="col-3" type="hidden" name="locationId" value="<?php echo $locationId; ?>" readonly>
+<input class="col-3" type="submit" value="Update stock!">
 </form>

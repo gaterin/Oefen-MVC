7c301537d3b7d4c846a90969bdbbe9fd3d7e5e85
Frank-4a5a <449758@edu.rocmn.nl>
2019-01-20 18:52:55 +0100

    removed graphcharts experiment on homepage

--- View/Home.php
+++ View/Home.php
@@ -2,20 +2,22 @@
 ?>
 <div class="row" id="homePage">
   <div class="col-12">
-    <div class="col-1"></div>
-    <div class="col-5">
+    <h1>HomePage</h1>
+    <div class="col-2"></div>
+    <!-- <div class="col-5">
       <h1>Stock Chart</h1>
       <canvas id="stockChart"></canvas>
-    </div>
+    </div> -->
 
     <!-- <div class="col-3"></div> -->
-    <div class="col-5">
+    <!-- <div class="col-5">
       <h1>Product Chart</h1>
       <canvas id="productChart"></canvas>
-    </div>
+    </div> -->
       <!-- <canvas id="myChart"></canvas> -->
-    <!-- <img class="col-8" src="./Images/HomeDepot.jpg"> -->
-    <div class="col-1">
+
+    <img class="col-8" src="./Images/HomeDepot.jpg">
+    <div class="col-2">
 
     </div>
   </div>
